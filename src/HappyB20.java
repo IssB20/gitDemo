02652c35696bca8a78484bb6de886299d75681d5
iss doc <islem_ouiddir@yahoo.fr>
2020-08-01 11:51:16 -0400

    update HappyB20.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -2,6 +2,7 @@ public class HappyB20 {
     public static void main(String[] args) {
 
         System.out.println("Happy Cybertek Students");
+        System.out.println("Best School ever");
 
     }
 }
--- src/HelloB20.java
+++ src/HelloB20.java
@@ -3,5 +3,6 @@ public class HelloB20 {
     public static void main(String[] args) {
 
         System.out.println("hello B20, ");
+        System.out.println("Hello world");
     }
 }
